6eba039a8a427a4aa32e3be6047c7a4c3289fd90
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 22:32:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,9 @@ def summon_captain_planet(veggies)
   veggie_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  i = 0
+  
 end
 
 def find_the_cheese# code an argument here
