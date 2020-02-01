377f0085b95061147a491c3bfa926215b01a61e4
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 22:49:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,8 @@ end
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
     cheddar_cheese.find do |type|
-      cheese_types.include 
-  
+    cheese_types.include? (type)
+  end
 end
 
 
