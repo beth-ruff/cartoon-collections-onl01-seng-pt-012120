1ba6541e4464a670a68a7f737397641e5b9b1096
Beth Ruff <beth.ruff2@gmail.com>
2020-02-01 22:12:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(dwarves)
- dwarves.each_with_index do |index, name| 
+ dwarves.each_with_index do |index, name|
+   puts "#{index} #{name}"
+ end
 end
 
 def summon_captain_planet# code an argument here
